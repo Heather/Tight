c9e37529fea1b9ce714c2a7bbc78acbc649ca382
Heather <heather@live.ru>
2015-07-27 13:02:42 +0400

    ffi example: haskell from C#

---
+++ .net/hs.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Foo {
+    class MainClass {
+        [DllImport("ffi.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void hs_init(IntPtr argc, IntPtr argv);
+
+        [DllImport("ffi.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void hs_exit();
+
+        [DllImport("ffi.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int foo(string str);
+
+        public static void Main(string[] args) {
+            Console.WriteLine("Initializing runtime...");
+            hs_init(IntPtr.Zero, IntPtr.Zero);
+
+            try {
+                Console.WriteLine("Calling to Haskell...");
+                int result = foo("C#");
+                Console.WriteLine("Got result: {0}", result);
+            } finally {
+                Console.WriteLine("Exiting runtime...");
+                hs_exit();
+            }
+        }
+    }
+}
--- build.bat
+++ build.bat
@@ -1,3 +1,38 @@
 @echo off
 
+echo HS
+ghc -O2 --make -no-hs-main -shared -o ffi.dll hs/ffi.hs
+:: -dynamic
+
+echo .NET
+if not "%VS120COMNTOOLS%"=="" (
+  set "VCVARSALL=%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
+  goto HaveVCVARSALL
+)
+for /F "usebackq skip=2 tokens=1,2*" %%A IN (`REG QUERY "%HKLMSoftwareFor32BitApps%\Microsoft\VisualStudio\12.0\Setup\VC" /v ProductDir 2^>nul`) DO (
+  set "VCVARSALL=%%Cvcvarsall.bat"
+)
+if not "%VCVARSALL%"=="" (
+  goto HaveVCVARSALL
+)
+if exist "%ProgramFilesFor32BitApps%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+  set "VCVARSALL=%ProgramFilesFor32BitApps%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+  goto HaveVCVARSALL
+)
+
+echo Visual Studio not found
+goto End
+
+:HaveVCVARSALL
+call "%VCVARSALL%" %VCVARSALLBitsOption%
+pushd .net
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe -unsafe hs.cs
+popd
+set LD_LIBRARY_PATH=. && ".net/hs.exe"
+
+echo RKT
 raco exe tight.rkt
+
+echo Done
+
+:End
