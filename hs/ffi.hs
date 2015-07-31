7308a94d41dcffe0eb289b1823d762bb29d8f8f4
Heather <heather@live.ru>
2015-07-31 17:47:53 +0400

    ∷

--- hs/ffi.hs
+++ hs/ffi.hs
@@ -1,19 +1,23 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE
+    OverloadedStrings
+  , UnicodeSyntax
+  #-}
+
 module Foo where
 
 import Foreign.C.String
 import Foreign.C.Types
 
 foreign export ccall
-  foo :: CString -> IO CInt
+  foo ∷ CString → IO CInt
 
-foo :: CString -> IO CInt
+foo ∷ CString → IO CInt
 foo c_str = do
-  str    <- peekCString c_str
-  result <- hs_foo str
+  str    ← peekCString c_str
+  result ← hs_foo str
   return $ fromIntegral result
 
-hs_foo :: String -> IO Int
+hs_foo ∷ String → IO Int
 hs_foo str = do
   putStrLn $ "Hello, " ++ str
   return (length str + 42)
